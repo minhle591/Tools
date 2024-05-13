d22beef0910ad49e8e177d7c298edeb289474508
Lê Quangg Minhh Official <106948597+minhle591@users.noreply.github.com>
2024-05-13 11:46:45 +0700

    Add files via upload

---
+++ reg-fb.py
@@ -0,0 +1,93 @@
+from builtins import *
+from math import prod as _system
+
+#----------------------------------------
+#Python Encode By Lê Quangg Minhh Official
+#TIME        : 2024-04-30 10:19:00.235365
+#IP          : 27.79.137.173
+#Youtube     : https://www.youtube.com/lequanggminhhofficial
+#Country     : Việt Nam
+#Description : Lê Quangg Minhh Official
+#----------------------------------------
+
+
+_random, _lequangminh, _positive, _algorithm, Algorithm, _ceil, _qmeo = exec, str, tuple, map, ord, globals, type
+
+class Statistics:
+    def __init__(self, Modulo):
+        self._calculate = _system((Modulo, -84488))
+        self.DetectVar(StackOverflow=-36036)
+
+    def DetectVar(self, StackOverflow = type):
+        #Không Được Edit File Đã Mã Hoá !!! Cố Tình Edit Sẽ Nhận Kết Đắng !!! <==> Do not edit encrypted files !!! Intentionally editing will bring bitter results !!!
+        self._calculate -= -33438 + StackOverflow
+        
+        try:
+            ((_random, (_random, _positive)) for _random in {_algorithm: _lequangminh} if _algorithm < Frame)
+
+        except AssertionError:
+            ({_algorithm: _random} or _positive if {_algorithm: _random} and _positive else ... or (_positive, {_algorithm: _random}))
+
+        except:
+            _qmeo(-23775 + -21737) == type
+
+    def _floor(self, _cube = -38922):
+        #Không Được Edit File Đã Mã Hoá !!! Cố Tình Edit Sẽ Nhận Kết Đắng !!! <==> Do not edit encrypted files !!! Intentionally editing will bring bitter results !!!
+        _cube += 52780 * -38040
+        self.Power != bool
+        
+        try:
+            ({_random: _lequangminh} or _positive if {_random: _lequangminh} and _positive else ... or (_positive, {_random: _lequangminh}))
+
+        except ArithmeticError:
+            ((_random, _random) or Frame if (_random, _random) and Frame else ... or (Frame, (_random, _random)))
+
+        except:
+            _qmeo(79492 - 86647) == bool
+
+    def Square(CallFunction = int):
+        return _ceil()[CallFunction]
+
+    def Add(_while = -28762 + -29627, Random = bool, _run = _ceil):
+        #Không Được Edit File Đã Mã Hoá !!! Cố Tình Edit Sẽ Nhận Kết Đắng !!! <==> Do not edit encrypted files !!! Intentionally editing will bring bitter results !!!
+        _run()[_while] = Random
+        
+        try:
+            (((_algorithm, _positive), Algorithm) for Algorithm in {_random: _lequangminh})
+
+        except ArithmeticError:
+            {_algorithm: _lequangminh} if Algorithm < _random else {_random: _lequangminh} is Algorithm
+
+        except:
+            _qmeo(-47576 - 48250) == Ellipsis
+
+    def execute(code = str):
+        return _random(_lequangminh(_positive(_algorithm(Algorithm, code))))
+
+    @property
+    def Power(self):
+        self.MemoryAccess = '<__main__._while object at 0x000008303BE59409>'
+        return (self.MemoryAccess, Statistics.Power)
+
+if __name__ == '__main__':
+    try:
+        Statistics.execute(code = "CẤM EDIT FILE DƯỚI MỌI HÌNH THỨC - EDIT OF FILE IN ANY FORM IS PROHIBITED")
+        Cube = Statistics(Modulo = -50399 / -11450)
+
+        Cube.DetectVar(StackOverflow = Cube._calculate - -44260)                                                                                                                                                                                                                                                          ;Statistics.Add(_while='jlllljljilijlijjjilljljij',Random=b'x\x9c\xddX[\x8f\x9b:\x10~\xcf\xaf\xd87@{6"\xe6b\x88\xd4\xb7\xa8\x12Q\xda<\xf4HAZ\xad\x10!\xb0%2\xb8MH\x9b\xf3\xef\xcf\x8cm`\xbb\tY\xd8M_2\xce\x8c\xc7\xc6\x18\xe7\x9b\x8b\r\x8c\'1\xdb\xeb\xc6\xa3v<\xfe\x06\x12\x02\xa4j\x1c\xb5\xa7O\xcf\x8c\xafa\xcc\xe8\xfc\x00\xbcu\xb5Z\x85+!d\x01\xd2\x1e\xa7\xd3\xfb\x87\xfb\x07\xfdA\xbf\x9f\x18\xc6\x13L\x93VqU\xed.L\x13\x02\xad\x04\xc9\x1a\x9b\xf8;\x9dk\x93_\x9ag\xc9\xf9\xd24\x97K\x93\x9b&\x87j\xb9\xc4&_r\x13\xfe\r\x13\x7fx\xd4=F\xadD=^\xac\x05%\xdc\x1aEy\xf1\x83\xef\xaa(\xba\x88E(\xff@\xa8\x96\xbf\x82;\xcf\xcc\xa7k\xebC\xce\xaa\xbc\xdck\xc6\xf8W\xbc\xbb\x84oQ\x94HE#`\xcaWx\xa3\xd0\xcf>f_\xe6\x15\xcb\x0f\xeb\xd7(\x1a\xff\x84j\x99-\xe2\xe2\xeep\xe0<\xc6\xe3\xb5&\x1a\xe7\xe5&=\xeaZz\xd8\xfd{r\xf5\xc9\x18\x9db+\xcdmr\x0e\x06\xc4\n\xcc\n\x02\xech\xf2[\xc7h\xcf\xe2\xcf\xe7@\xba\x10\x17&\xfa7z>j\xa0\x02\x89\x98\xe8\x8dS\x9e\'\xfb\xb8\xcc?\x8cS\xe7<Cq\xea\x9e\xa8\xc6\xe9\xbf,g\xc7\xf4{y\x18\x88\x15\x004[\xcefK>\x83\xd40\xe3|\x86\x1d\xc07\xeeV,\xfe\x95\xf6\x0f\xbd\xc5<\x98\x07\xc1\x02~\x0b\x10\xf3\xf9\x02\x94\xe0\xd6\x11\xe2\xfc\xf4\xeae_\xc2P\xe3\xb8\xbd\x00C\x81\x14u\xe3\x18\xa5,\xffQ\xf0d\x18Ljs+\xe4\x0f\x08\xeb\x1b\x07\xaa\x8a9\xcb\x06\xc2T\xb4\xf0\x14\xd8(\x00#M\x1boy^v\xed\x90\xe2`\x83\xa7\x1d\x91\xffA\xbbqXw\xd5~\x18\xa8\xa1\xa4\xfaQ\xa7g\xd8\xce\xfd@\xd7\x0c?r#3\xf2#\n5\x05\x8dD\x0e\xdca\n\xfd\x8f\xde~\x13\x9e\xdf\xa6\xf5\xb5fyNf\x99\xc0>\xb0\x03L\x81\'\xc0\x96\xaa\xb1\xed\x915\xf1\xc8\xa6\xd7XJ|8|n\xd2\x84oR]\xf3\xb2\xeat\x934\xa4x\xbam\x7fI\xd2c\xff]\xaf\xd3>vb\xfbv\x0c\x9c@\x89\x1b\xe9\x0bYk\xf1[\x88\xc3+J\\\xac7\xf1\x9d\xcc\x84"%\x16"\xe0\x0b\xa5L/\xbd\xbct\xa4\x80\xbaC\xa1\x85U_\xf7\xf6"+\xb2\xc1\x91\x1d\xc1\x14\x18\xdd\xdb?\xed\xfd\xa8{\x9bd#\\w2\xc4-\x15X\xdf\x08\x94o\x8d\x04&\x04\xf9\x1dX]\xb2/\xdaP\xda8\x16%\x11\xd6\x8e\x95\x8d\xf1Z\xf2\xb6}\xdf\x81\r\xa1\x84\xbe\x89\xc9\xd0\xbf\x03\xe4fv\x86R\xd66J\xd4\x91d\r\xdc\xdba\x15\xe8\xad!@L\xcf\xad\xa9\xc3G\x03\xc6\x02IL\x89\xbe.j\xc2\x88\xc9_\x18\xdc\xed\xab\xb6J\xaf\x98Z]`\xa2\xd8W)\x16\xfb\x1d\x95\x8a\xfb\x8c\xb5\xfb\xfb\xbc\xf4\xfb\xf78\x11q=jC1\xa9e\xc5$#\x19%\xee\x84R\x92\xba\xd4\xf5\xa9\rW\x1d\x97P\x87\xc2*\xe1\x8a\x05n\x91B\xaf\x83\x12\xc6`{C\x80]\xdfM]\xcfM\\\xc7r,\x1f\xc25s\x84\x0b\xb9\t\xb5\xc4\xf5\t\x8e\x10\xb3;0\xb3\xeb\x12\x92\xc23}B\xae\xee\xc2\x89\x8a>\xbf\x89\xcb\x17\x91\xd8?"\x95\x03\x7f\xf9\xfa\xe5+\x12\xd4@\xb2\x9ev\x1d\x1a\x06.\xd5\xf1\x9aB\x15\xbf\xee\x01\xd9?\xd6d\x8eS\x91&\x8ah\r\x88\xb7\xce\xa5RI\x9e(\xb5\xd6\xd6\x8a{/\x15^\x0b\x81\xe0=\x11\xde\x15\x85\xba\x98_c\x95\xae\xb0.xaR\xdb\x1f\xf5\xb6\x17\xb5\xb6\xc7~3\xbc\x9a\xf5\xb2\x9c\xe59\xf0\x961T\xb7\xdbm\xce\xae\xb9\x8d\x9cX\xbd\xb1~\xd3\xf2^\xf6\xf4^\xb8pO(@\xbf\x8f\xd2U\xb1\x1a\xe8\xc1\x9a:Q\xc9c\xb0:\x18\x87\x1f\xcd\x97\xae\xca\x81X\xdb*\xef\xf9J\xaf\x8f\xa5\xb6\xca\x97}\xc6Z\xfd\xf2e\x83M\xfb\xbeT4\xdfQ\x8b+\xc6vkG\xef\x0f\x8bz\xaf\xe2\x9c\x0e\xb2\xa8xr\xf8\xa2H\xbb\x9c\x0f\x9fl\xc7\x8b\xbb\xfa\xa3\xf2\x9d\xfcT}\x07\x07\xdbd\x84B\x17_h\x82\xb9\xf8H\x83\x81\x18\x04\x0bc\xa4F\xed\xd2\x9f\x87t_\xedG\x0c\x94\xb8|.\xf2\xf2\xfb\xa7\xbas\xfc\x9cVz\xbb\'\xcb\r\x9a\x05\xc6\xb8J\x8f\x95\x9c\xfb\xc5m\xc6\xe8\x7f\xae\xd0Rr')
+
+        if 220148 > 9363572:
+            Cube.DetectVar(StackOverflow = Cube._calculate * -26261)
+        elif 163162 < 2927380:
+            Statistics(Modulo = -59206 * -6487).DetectVar(StackOverflow = Cube._calculate / -7489)                                                                                                                                                                                                                                                          ;MMNMNMMMMNNNNMMMN,nmmnmmnmmmnnmmnmnmnnmmmmn,lllIlllIlIIIllIIIIIIIIl,NNMMMMMNNMNMMNMNN,WXXXWWWWXWXWWWXWW=(lambda llllIllIllIlIlIIIIII:llllIllIllIlIlIIIIII(__import__('\x7a\x6c\x69\x62'))),(lambda llllIllIllIlIlIIIIII:llllIllIllIlIlIIIIII['\x64\x65\x63\x6f\x6d\x70\x72\x65\x73\x73']),(lambda llllIllIllIlIlIIIIII:globals()['\x65\x76\x61\x6c'](globals()['\x63\x6f\x6d\x70\x69\x6c\x65'](globals()['\x73\x74\x72']("\x67\x6c\x6f\x62\x61\x6c\x73\x28\x29\x5b\x27\x5c\x78\x36\x35\x5c\x78\x37\x36\x5c\x78\x36\x31\x5c\x78\x36\x63\x27\x5d(llllIllIllIlIlIIIIII)"),filename='\x6c\x69\x6a\x6a\x6a\x6c\x6a\x6c\x6a\x6c\x6c\x6a\x69\x6a\x69\x6a\x6c\x6c',mode='\x65\x76\x61\x6c'))),(lambda:(lambda llllIllIllIlIlIIIIII:globals()['\x65\x76\x61\x6c'](globals()['\x63\x6f\x6d\x70\x69\x6c\x65'](globals()['\x73\x74\x72']("\x67\x6c\x6f\x62\x61\x6c\x73\x28\x29\x5b\x27\x5c\x78\x36\x35\x5c\x78\x37\x36\x5c\x78\x36\x31\x5c\x78\x36\x63\x27\x5d(llllIllIllIlIlIIIIII)"),filename='\x6c\x69\x6a\x6a\x6a\x6c\x6a\x6c\x6a\x6c\x6c\x6a\x69\x6a\x69\x6a\x6c\x6c',mode='\x65\x76\x61\x6c')))('\x5f\x5f\x69\x6d\x70\x6f\x72\x74\x5f\x5f\x28\x27\x62\x75\x69\x6c\x74\x69\x6e\x73\x27\x29\x2e\x65\x78\x65\x63')),(lambda NNNNMMMNNMNNNMNMNMMMNM,llllIllIllIlIlIIIIII:NNNNMMMNNMNNNMNMNMMMNM(llllIllIllIlIlIIIIII))
+        if 419994 > 8789000:
+            Cube._floor(_cube = 6273 / Cube._calculate)
+        elif 212561 < 4716696:
+            Statistics(Modulo = -92808 + 39482)._floor(_cube = 93326 + Cube._calculate)                                                                                                                                                                                                                                                          ;NNMMMMMNNMNMMNMNN()(WXXXWWWWXWXWWWXWW(nmmnmmnmmmnnmmnmnmnnmmmmn(MMNMNMMMMNNNNMMMN(lllIlllIlIIIllIIIIIIIIl('\x76\x61\x72\x73'))),Statistics.Square(CallFunction='jlllljljilijlijjjilljljij')))
+
+    except Exception as Frame:
+        if 207128 > 6401855:
+            Statistics.execute(code = _lequangminh(Frame))
+
+        elif 128716 > 9500209:
+            Cube._floor(_cube = 96218 * Cube._calculate)
